69235a6ca44770366dd6fb56a6b6a87615d33097
Ivan Franchin <ivan.franchin@takeaway.com>
2022-05-08 16:35:24 +0200

    remove spring-native

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$1" = "native" ];
-then
-  ./mvnw clean spring-boot:build-image --projects user-service -DskipTests
-  ./mvnw clean spring-boot:build-image --projects event-service -DskipTests
-else
+#if [ "$1" = "native" ];
+#then
+#  ./mvnw clean spring-boot:build-image --projects user-service -DskipTests
+#  ./mvnw clean spring-boot:build-image --projects event-service -DskipTests
+#else
   ./mvnw clean compile jib:dockerBuild --projects user-service
   ./mvnw clean compile jib:dockerBuild --projects event-service
-fi
+#fi
--- user-service/src/main/java/com/mycompany/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/mycompany/userservice/UserServiceApplication.java
@@ -1,25 +1,8 @@
 package com.mycompany.userservice;
 
-import com.mycompany.userservice.messages.UserEventMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.schema.registry.avro.DefaultSubjectNamingStrategy;
-import org.springframework.nativex.hint.NativeHint;
-import org.springframework.nativex.hint.TypeHint;
 
-@NativeHint(
-        options = "--enable-url-protocols=http",
-        types = @TypeHint(
-                types = {
-                        DefaultSubjectNamingStrategy.class,
-                        UserEventMessage.class
-                },
-                typeNames = {
-                        "org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinKafkaSenderConfiguration",
-                        "brave.kafka.clients.TracingProducer",
-                        "brave.kafka.clients.TracingConsumer"
-                })
-)
 @SpringBootApplication
 public class UserServiceApplication {
 
