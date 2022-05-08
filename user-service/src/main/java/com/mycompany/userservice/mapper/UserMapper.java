740c9411596e440997b1e55f00d4b3c4ba5bcd28
Ivan Franchin <ivan.franchin@takeaway.com>
2022-05-08 15:54:31 +0200

    project upgrade - upgrade to spring-boot 2.6.7; - upgrade to spring-cloud 2021.0.2; - upgrade to springdoc-openapi 1.6.8; - upgrade to openjdk-image 11.0.15; - upgrade to mysql docker image 5.7.38; - add Mapping annotation to ignore some fields in Mapper class.

--- user-service/src/main/java/com/mycompany/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/com/mycompany/userservice/mapper/UserMapper.java
@@ -1,26 +1,31 @@
 package com.mycompany.userservice.mapper;
 
+import com.mycompany.userservice.model.User;
 import com.mycompany.userservice.rest.dto.CreateUserRequest;
 import com.mycompany.userservice.rest.dto.UpdateUserRequest;
 import com.mycompany.userservice.rest.dto.UserResponse;
-import com.mycompany.userservice.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import org.mapstruct.ReportingPolicy;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Mapper(
         componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     User toUser(CreateUserRequest createUserRequest);
 
     UserResponse toUserResponse(User user);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     void updateUserFromRequest(UpdateUserRequest updateUserRequest, @MappingTarget User user);
 }
